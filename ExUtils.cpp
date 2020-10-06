07a9c08052864c961813792216370d62410bf362
Zhang Bowen <chambowen@126.com>
2020-10-07 01:38:46 +0800

    add p-value <-> Z

--- ExUtils.cpp
+++ ExUtils.cpp
@@ -1,9 +1,9 @@
 #include "ExUtils.h"
 #include "Utils.h"
 
-using namespace SP;
+#include "TMath.h"
 
-double SP::GetChiSquare(TH1* h, TF1* f)
+double SPEx::GetChiSquare(TH1* h, TF1* f, bool useRootStyle)
 {
     // definition:
     //
@@ -33,8 +33,11 @@ double SP::GetChiSquare(TH1* h, TF1* f)
         double x2_i = 0.0;
         double obs_i = h->GetBinContent(i);
         double exp_i = f->Eval(h->GetXaxis()->GetBinCenter(i));
-        if (obs_i != 0)
-            x2_i = (obs_i - exp_i) * (obs_i - exp_i) / std::fabs(obs_i);
+        if (obs_i > 1e-12)  // is zero
+            if (useRootStyle)
+                x2_i = (obs_i - exp_i) * (obs_i - exp_i) / std::fabs(obs_i);
+            else
+                x2_i = (obs_i - exp_i) * (obs_i - exp_i) / std::fabs(exp_i);
         else
         { 
             x2_i = 0;
@@ -49,3 +52,12 @@ double SP::GetChiSquare(TH1* h, TF1* f)
     return x2_sum;
 }
 
+double SPEx::GetSignificance(double pvalue)
+{
+    return sqrt(2.0) * TMath::ErfInverse(1.0 - 2.0 * pvalue);
+}
+
+double SPEx::GetPvalue(double z)
+{
+    return 0.5 * (1.0 - TMath::Erf(z / sqrt(2.0)));
+}
--- ExUtils.h
+++ ExUtils.h
@@ -14,11 +14,33 @@
 #include "TF1.h"
 
 
-namespace SP
+namespace SPEx
 {
-    double GetChiSquare(TH1* h, TF1* f);
+    /**
+     * @brief Calculate chi square value using observed data from histogram
+     * and expected data from function, use root style by default, can switch
+     * to the actual definition.
+     */
+    double GetChiSquare(TH1* h, TF1* f, bool useRootStyle = true);
 
+    /**
+     * @brief Calculate significance given p-value
+     *
+     *             -1
+     * Z = √2 x erf  (1 - 2p)
+     *
+     */
+    double GetSignificance(double pvalue);
 
+    /**
+     * @brief Calculate p-value given significance
+     *
+     *     1               Z
+     * p = - x [ 1 - erf (---)]
+     *     2              √2
+     *
+     */
+    double GetPvalue(double z);
 }
 
 #endif // EXUTILS_H
